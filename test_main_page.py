c8cde5caeb358c3f4f14a120f56b8d96412a28e1
Skan4ik <skan4ik1337@yandex.ru>
2023-04-13 11:39:09 +0500

    change function and new import from mainpage

--- test_main_page.py
+++ test_main_page.py
@@ -2,10 +2,11 @@ import time
 from selenium import webdriver
 import math
 from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
+
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    time.sleep(10)
-    login_link.click()
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
