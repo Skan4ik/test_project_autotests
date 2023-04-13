cf4fd1ddc088b615162c771bc14e86127fbd15e0
Skan4ik <skan4ik1337@yandex.ru>
2023-04-13 12:22:09 +0500

    add new file login_page and class LoginPage with function in login url, login form and register form

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from selenium.webdriver.common.by import By
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        current_url = self.browser.current_url
+        assert "Java" in current_url, "It isn't a login link"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login link is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTR_FORM), "Register form is not presented"
